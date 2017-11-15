c87bc746e576d2a02de28e6f42e6288b051df919
Xarboule <remi.dulong@telecom-sudparis.eu>
2017-11-15 19:49:33 +0100

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include "Srf02.hpp"
+
+using namespace std;
+
+int main(){
+
+	cout << "---- srf02 sensors utility ----" << endl;
+
+	return EXIT_SUCCESS;
+
+}
