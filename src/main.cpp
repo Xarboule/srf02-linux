2e2f5994e461c153888c6769c99cd38bc30e4e74
Remi Dulong <remi.dulong@telecom-sudparis.eu>
2017-11-19 11:52:06 +0100

    option parsing with getopt

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,45 @@
 #include <stdio.h>
 #include <iostream>
 #include "Srf02.hpp"
-#include <argp.h>
+#include <unistd.h>
 
 using namespace std;
 
-static char doc[] = "Usage : srf02-utility [-c current_addr new addr]";
-
-static struct argp argp = { 0, 0, 0, doc };
 
 int main (int argc, char **argv){
 
+	char optstring[] = "c"; //allowed options
+	int c;
+	int cflag = 0; //if -c option detected
 
-	argp_parse (&argp, argc, argv, 0, 0, 0);
-	
 	cout << "---- srf02 sensors utility ----" << endl;
+
+	while((c=getopt(argc, argv, optstring))!=-1){
+		switch (c)
+		{
+			case 'c':
+				cflag = 1;
+				break;
+			case '?':
+				if (optopt == 'c')
+					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				else if (isprint (optopt))
+					fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+				else
+					fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+				return 1;
+			default:
+				abort();
+		}
+	}
+	//printf("%s\n", argv[optind]);
+
+	if (cflag) {
+		char *old_addr = argv[optind];
+		char *new_addr = argv[optind+1];
+		cout << "Changing sensor ID from " << hex << old_addr << " to "<< hex << new_addr << endl;
+	}
+
 	return EXIT_SUCCESS;
 
 }
