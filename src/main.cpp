2b0180f7d07bd3dd961dc42a69706b3c27c61f02
Xarboule <remi.dulong@telecom-sudparis.eu>
2017-11-19 11:14:10 +0100

    test argp

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,20 @@
 #include <stdio.h>
 #include <iostream>
 #include "Srf02.hpp"
+#include <argp.h>
 
 using namespace std;
 
-int main(){
+static char doc[] = "Usage : srf02-utility [-c current_addr new addr]";
 
+static struct argp argp = { 0, 0, 0, doc };
+
+int main (int argc, char **argv){
+
+
+	argp_parse (&argp, argc, argv, 0, 0, 0);
+	
 	cout << "---- srf02 sensors utility ----" << endl;
-
 	return EXIT_SUCCESS;
 
 }
